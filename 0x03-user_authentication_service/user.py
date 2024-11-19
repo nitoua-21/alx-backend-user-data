2a81a387baab144070a8d839cd83977b9eefbde9
Normich ITOUA <nitoua.dev@gmail.com>
2024-11-19 23:09:15 +0100

    Update task 0

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -18,10 +18,3 @@ class User(Base):
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250))
     reset_token = Column(String(250))
-
-
-if __name__ == '__main__':
-    print(User.__tablename__)
-
-    for column in User.__table__.columns:
-        print("{}: {}".format(column, column.type))
