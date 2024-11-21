f1733671b3d149b3bf8aa361247847d0cc7d97b2
Normich ITOUA <nitoua.dev@gmail.com>
2024-11-21 18:08:57 +0100

    Add task !@

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,6 +4,7 @@ Module auth
 """
 import bcrypt
 import uuid
+from typing import Union
 from sqlalchemy.orm.exc import NoResultFound
 from db import DB, User
 
@@ -61,3 +62,12 @@ class Auth:
         session_id = _generate_uuid()
         self._db.update_user(user.id, session_id=session_id)
         return session_id
+
+    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
+        """Get a user from session id
+        """
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
