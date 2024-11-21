04db53178c08c70d5d75625e7575c6e0ac9b3c5a
Normich ITOUA <nitoua.dev@gmail.com>
2024-11-21 18:17:35 +0100

    Add task 13

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -71,3 +71,10 @@ class Auth:
             return user
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """Destroy session for a user given ID
+        """
+        if user_id is None:
+            return None
+        self._db.update_user(user_id, session_id=None)
